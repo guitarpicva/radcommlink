97d4b1225ed97be31814c6e09b0240bf3c073db1
guitarpicva <ab4mw@radcommsoft.com>
2019-09-19 14:43:32 -0400

    updated source with license data

--- stationlistdialog.cpp
+++ stationlistdialog.cpp
@@ -1,3 +1,19 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) 2019 GrizzWorks, LLC
+// ALL RIGHTS RESERVED
+//
+// This file is part of radcommlink software project under the GNU GPLv3
+// license.
+//
+// radcommlink is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// Parts of radcommlink may contain source code from Open Source projects.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "stationlistdialog.h"
 #include "ui_stationlistdialog.h"
 
